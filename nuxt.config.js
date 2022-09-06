794da19ab737c4ec354cc54cec2891cdba48db60
Sam Li <weisen.li@hotmail.com>
2022-09-05 22:01:46 -0400

    feat(homepage): render list from movie api

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module',
+    '@nuxtjs/stylelint-module'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -42,13 +42,13 @@ export default {
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
-    '@nuxt/content',
+    '@nuxt/content'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: '/'
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,29 @@
 <template>
-  <Tutorial/>
+  <main>
+    <h1>What Sam Watched in Nuxt.js</h1>
+    <article v-for="v in visuals" :key="v.id">
+      <h3>{{ v.title }}</h3>
+    </article>
+  </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      visuals: []
+    }
+  },
+  async fetch () {
+    const API = 'https://what-i-watched.herokuapp.com/api/visuals?limit=20'
+    const response = await fetch(API).then(res => res.json())
+    this.visuals = response.results
+  },
+  mounted () {
+    this.$fetch()
+  }
 })
 </script>
