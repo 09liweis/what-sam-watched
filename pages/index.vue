10b82d4686e5d8490655cab47dce10edce460117
Sam Li <weisen.li@hotmail.com>
2022-09-07 11:22:12 -0400

    style(home): apply some tailwind style to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <main>
-    <h1>What Sam Watched in Nuxt.js</h1>
+  <main class="conatiner">
+    <h1 class="text-xl text-center text-red-500 font-bold mb-8">What Sam Watched in Nuxt.js</h1>
     <article v-for="v in visuals" :key="v.id">
-      <h3>
+      <h3 class="font-mono text-red-400 hover:text-red-900 underline decoration-blue-300 hover:decoration-blue-400">
         <NuxtLink :to="{ name: 'detail-id', params: { id: v.id }}">{{ v.title }}</NuxtLink>
       </h3>
     </article>
