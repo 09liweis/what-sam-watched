19647c5d9ef27a1a0eeac7341cc9edf0c32c8e53
Sam Li <weisen.li@hotmail.com>
2022-09-07 11:21:26 -0400

    feat(head): update head title/content, link

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,15 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'what-sam-watched',
+    title: 'What Sam Watched Dashboard',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Just a dashboard management tool' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
+      {rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css'},
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
