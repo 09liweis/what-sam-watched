7c6624c13211c8aa2b05a5a18d422fd92629374c
Sam Li <weisen.li@hotmail.com>
2022-10-28 22:12:42 -0400

    head(config): add head config to app config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,18 @@ export default defineNuxtConfig({
   nitro: {
     plugins: ["~/server/index.ts"],
   },
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width, initial-scale=1', 
+      title: 'What Sam Watched',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'My amazing movie dashboard.' }
+      ],
+      link:[
+        {href :'https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css',rel:'stylesheet'}
+      ]
+    }
+  }
 })
