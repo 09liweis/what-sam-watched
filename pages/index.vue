6f951dd5bd260145d51ff52666ac2b85e5fdf2c3
Sam Li <weisen.li@hotmail.com>
2022-11-12 00:28:32 -0500

    feat(home): update visual episode

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
+<script setup>
+const { data } = await useFetch('/api/visuals')
+const {visuals} = data.value;
+
+async function updateEpisode(v) {
+  await useFetch(`/api/update?id=${v._id}`);
+}
+</script>
 <template>
   <NuxtLayout>
     <main class="conatiner">
@@ -5,13 +13,10 @@
       <article v-for="v in visuals" :key="v.id">
         <h3 class="font-mono text-red-400 hover:text-red-900 underline decoration-blue-300 hover:decoration-blue-400">
           <NuxtLink :to="{ name: 'id', params: { id: v._id }}">{{ v.title }}</NuxtLink>
+          <span class="ml-8 mr-8">{{v.current_episode}}/{{v.episodes}}</span>
+          <button @click="updateEpisode(v)">+1</button>
         </h3>
       </article>
     </main>
   </NuxtLayout>
 </template>
-
-<script setup>
-const { data } = await useFetch('/api/visuals')
-const {visuals} = data.value;
-</script>
