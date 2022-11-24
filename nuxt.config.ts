324309452900dd2289a85700bfb92ea2a544191e
Sam Li <weisen.li@hotmail.com>
2022-11-24 14:26:28 -0500

    render(deploy): add config for render

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  server: {
+    host: '0.0.0.0'
+  },
   runtimeConfig: {
     mongoUrl: process.env.MONGO_URL,
   },
