f22b8aeaba4b80abd1e5986a3903a3ecef64ffc0
Sam Li <weisen.li@hotmail.com>
2022-11-25 16:13:58 -0500

    index(visual): update home style

--- pages/index.vue
+++ pages/index.vue
@@ -29,17 +29,19 @@ async function searchAndUpsert() {
 </script>
 <template>
   <NuxtLayout>
-    <main class="conatiner">
+    <main class="conatiner p-5">
       <h1 class="text-xl text-center text-red-500 font-bold mb-8">What Sam Watched in Nuxt.js</h1>
       <input placeholder="Enter Douban Id" v-model="input" />
       <button @click="searchAndUpsert()">Add</button>
-      <article v-for="v in visuals" :key="v.id">
-        <h3 class="font-mono text-red-400 hover:text-red-900 underline decoration-blue-300 hover:decoration-blue-400">
+      <article v-for="v in visuals" :key="v.id" class="flex justify-between mb-1.5">
+        <h3 class="font-mono text-red-400 hover:text-red-900 decoration-blue-300 hover:decoration-blue-400">
           <NuxtLink :to="{ name: 'id', params: { id: v._id }}">{{ v.title }}</NuxtLink>
           <span class="ml-8 mr-8">{{v.current_episode}}/{{v.episodes}}</span>
-          <button @click="updateVisual(v)">Update</button>
-          <button @click="updateEpisode(v)">+1</button>
         </h3>
+        <div>
+          <button class="mr-1.5" @click="updateVisual(v)">Update</button>
+          <button class="border px-1.5 border-amber-600 rounded" @click="updateEpisode(v)">+1</button>
+        </div>
       </article>
     </main>
   </NuxtLayout>
