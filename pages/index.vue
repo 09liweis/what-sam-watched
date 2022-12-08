2492e0c95ca61bd8487c5e38e6ac562ad91be90e
Sam Li <weisen.li@hotmail.com>
2022-12-08 14:10:16 -0500

    feat(home): add loading to add

--- pages/index.vue
+++ pages/index.vue
@@ -24,11 +24,13 @@ async function searchAndUpsert() {
   if (!douban_id) {
     return;
   }
+  loading = true;
   const body = {douban_id}
   await $fetch('/api/upsert',{
     method:'POST',
     body
   });
+  loading = false;
   const responseData = await $fetch('/api/visuals');
   visuals = responseData.visuals;
 }
@@ -38,7 +40,7 @@ async function searchAndUpsert() {
     <main class="conatiner p-5">
       <h1 class="text-xl text-center text-red-500 font-bold mb-8">What Sam Watched in Nuxt.js</h1>
       <input placeholder="Enter Douban Id" v-model="input" />
-      <button @click="searchAndUpsert()">Add</button>
+      <button @click="searchAndUpsert()">{{loading?'Loading':'Add'}}</button>
       <article v-for="v in visuals" :key="v.id" class="flex justify-between mb-1.5">
         <h3 class="font-mono text-red-400 hover:text-red-900 decoration-blue-300 hover:decoration-blue-400">
           <NuxtLink :to="{ name: 'id', params: { id: v._id }}">{{ v.title }}</NuxtLink>
