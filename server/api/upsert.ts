449977b19b5dfe04bcaafd8210c02cb81ccf59c0
09liweis <weisen.li@hotmail.com>
2023-05-03 14:34:09 -0400

    fix useBody to readBody

--- server/api/upsert.ts
+++ server/api/upsert.ts
@@ -1,17 +1,26 @@
-import {visualModel} from '../model/index';
+import { visualModel } from '../model/index';
 
-export default defineEventHandler(async(event)=>{
-  const body = await useBody(event);
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
   if (!body.douban_id) {
-    return {msg:'No douban id'};
+    return { msg: 'No douban id' };
   }
   const visualSummary = await $fetch(process.env.FETCH_VISUAL, {
     method: 'POST',
-    body
+    body,
   });
-  const {douban_id,douban_rating,poster,imdb_id,imdb_rating,visual_type,title,episodes} = visualSummary;
+  const {
+    douban_id,
+    douban_rating,
+    poster,
+    imdb_id,
+    imdb_rating,
+    visual_type,
+    title,
+    episodes,
+  } = visualSummary;
   const update = {
-    _id:body._id,
+    _id: body._id,
     douban_id,
     douban_rating,
     title,
@@ -20,13 +29,16 @@ export default defineEventHandler(async(event)=>{
     poster,
     episodes,
     visual_type,
-    date_updated:new Date(),
-    date_watched:new Date()
-  }
+    date_updated: new Date(),
+    date_watched: new Date(),
+  };
   if (update._id) {
     delete update.date_watched;
   }
-  const filter = {douban_id:update.douban_id};
-  const doc = await visualModel.findOneAndUpdate(filter, update, { new: true, upsert: true });
+  const filter = { douban_id: update.douban_id };
+  const doc = await visualModel.findOneAndUpdate(filter, update, {
+    new: true,
+    upsert: true,
+  });
   return doc;
 });
