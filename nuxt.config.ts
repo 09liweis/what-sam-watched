4bdbd46b52680ea17a4449260fb5a81e27825722
09liweis <weisen.li@hotmail.com>
2023-05-03 15:12:12 -0400

    feat(cors): add route rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,7 @@ export default defineNuxtConfig({
   },
   routeRules: {
     '/api/visuals': { cors: true },
+    '/api/visual': { cors: true },
     '/api/upsert': { cors: true },
   },
   image: {
--- pages/[id].vue
+++ pages/[id].vue
@@ -1,14 +1,16 @@
 <template>
   <main>
-    <h1>{{visual.title}}</h1>
-    <p>{{visual.imdb_id}}</p>
-    <p>{{visual.douban_id}}</p>
-    <p>{{visual.douban_rating}}</p>
-    
+    <h1>{{ visual.title }}</h1>
+    <p>{{ visual.imdb_id }}</p>
+    <p>{{ visual.douban_id }}</p>
+    <p>{{ visual.douban_rating }}</p>
   </main>
 </template>
 <script setup>
+const runtimeConfig = useRuntimeConfig();
+const API_HOST = runtimeConfig.public.apiHost || '';
+
 const route = useRoute();
-const { data } = await useFetch(`/api/visual?id=${route.params.id}`);
-const {visual} = data.value;
+const { data } = await useFetch(`${API_HOST}/api/visual?id=${route.params.id}`);
+const { visual } = data.value;
 </script>
