d16907afd4f74516895e0e3492ffd82a8d0f4dfd
09liweis <weisen.li@hotmail.com>
2023-05-03 12:35:04 -0400

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,34 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
   },
   runtimeConfig: {
     mongoUrl: process.env.MONGO_URL,
+    API_HOST: process.env.API_HOST || '',
   },
   nitro: {
-    plugins: ["~/server/index.ts"],
+    plugins: ['~/server/index.ts'],
   },
-  routeRules:{
+  routeRules: {
     '/api/visuals': { cors: true },
   },
   image: {
-    domains: ['https://img9.doubanio.com']
+    domains: ['https://img9.doubanio.com'],
   },
   app: {
     head: {
       charset: 'utf-16',
-      viewport: 'width=device-width, initial-scale=1', 
+      viewport: 'width=device-width, initial-scale=1',
       title: 'What Sam Watched',
       meta: [
         // <meta name="description" content="My amazing site">
-        { name: 'description', content: 'My amazing movie dashboard.' }
+        { name: 'description', content: 'My amazing movie dashboard.' },
       ],
       // link:[
       //   {href :'https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css',rel:'stylesheet'}
       // ],
-      script:[
-        {src:'https://cdn.tailwindcss.com'}
-      ]
-    }
-  }
-})
+      script: [{ src: 'https://cdn.tailwindcss.com' }],
+    },
+  },
+});
