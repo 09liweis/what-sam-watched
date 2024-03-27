be33790ebf054cbdda5924bc602f30ad2ab92088
09liweis <weisen.li@hotmail.com>
2024-03-27 16:59:51 -0400

    fix movie poster

--- pages/movies/[name].vue
+++ pages/movies/[name].vue
@@ -3,10 +3,10 @@
     <Loading color="text-red-500" v-if="loading" />
     <section class="flex flex-wrap" v-else>
       <article v-for="movie in moviesStore.movieList" class="w-1/2 sm:1/3 md:w-1/4 lg:w-1/6 p-2 transition duration-200 hover:scale-105">
-        <video v-if="movie.video" autoplay loop>
+        <video v-if="movie.video" autoplay loop class="movie-poster">
           <source :src="movie.video" type="video/mp4">
         </video>
-        <NuxtImg v-else :src="movie.poster" width="100%" :alt="movie.title" placeholder="https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg" loading="lazy" class="w-full rounded aspect-[3/4] bg-gray-100" />
+        <NuxtImg v-else :src="movie.poster" width="100%" :alt="movie.title" placeholder="https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg" loading="lazy" class="movie-poster" />
         <h3>{{ movie.title }}</h3>
       </article>
     </section>
