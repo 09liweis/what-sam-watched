bfdd4619b866609f9117624afbd230248543ff52
09liweis <weisen.li@hotmail.com>
2024-03-27 09:10:06 -0400

    style poster

--- pages/movies/[name].vue
+++ pages/movies/[name].vue
@@ -1,8 +1,8 @@
 <template>
-  <main class="flex flex-wrap gap-2">
+  <main class="flex flex-wrap">
     <Loading color="text-red-500" v-if="loading" />
-    <article v-for="movie in moviesStore.movieList" class="w-1/4">
-      <NuxtImg v-if="movie.poster" :src="movie.poster" width="25%" :alt="movie.title" loading="lazy" class="rounded hidden sm:block aspect-[3/4] bg-gray-100" />
+    <article v-for="movie in moviesStore.movieList" class="w-1/2 sm:1/3 md:w-1/4 lg:w-1/6 p-2">
+      <NuxtImg v-if="movie.poster" :src="movie.poster" width="100%" :alt="movie.title" loading="lazy" class="w-full rounded aspect-[3/4] bg-gray-100" />
     </article>
   </main>
 </template>
