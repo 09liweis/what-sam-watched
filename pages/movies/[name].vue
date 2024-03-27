b1c15e253f54234dddd537a0d926329b7bd6494f
09liweis <weisen.li@hotmail.com>
2024-03-27 12:01:02 -0400

    style moive item on movies page

--- pages/movies/[name].vue
+++ pages/movies/[name].vue
@@ -1,12 +1,14 @@
 <template>
-  <main class="flex flex-wrap">
+  <main class="flex justify-center items-center max-h-max">
     <Loading color="text-red-500" v-if="loading" />
-    <article v-for="movie in moviesStore.movieList" class="w-1/2 sm:1/3 md:w-1/4 lg:w-1/6 p-2">
-      <video v-if="movie.video" autoplay loop>
-        <source :src="movie.video" type="video/mp4">
-      </video>
-      <NuxtImg v-else :src="movie.poster" width="100%" :alt="movie.title" loading="lazy" class="w-full rounded aspect-[3/4] bg-gray-100" />
-    </article>
+    <section class="flex flex-wrap" v-else>
+      <article v-for="movie in moviesStore.movieList" class="w-1/2 sm:1/3 md:w-1/4 lg:w-1/6 p-2 transition duration-200 hover:scale-105">
+        <video v-if="movie.video" autoplay loop>
+          <source :src="movie.video" type="video/mp4">
+        </video>
+        <NuxtImg v-else :src="movie.poster" width="100%" :alt="movie.title" loading="lazy" class="w-full rounded aspect-[3/4] bg-gray-100" />
+      </article>
+    </section>
   </main>
 </template>
   <style></style>
