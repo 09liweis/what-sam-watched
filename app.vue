d38ee8edbc2506a88175a735aa6b7b93b49487c5
09liweis <weisen.li@hotmail.com>
2024-03-28 09:07:22 -0400

    refactor menu

--- app.vue
+++ app.vue
@@ -1,16 +1,23 @@
 <script setup>
 import { ref, computed, watch } from 'vue';
 const route = useRoute();
-let routeName = ref(route.params?.name);
+const SAM = 'sam';
+const getRouteName = (route) => {
+  return route.params?.name || SAM;
+}
+let routeName = ref(getRouteName(route));
 const ROUTES = [
-  {id:'sam',to:'/',nm:'Sam'},
+  {id:SAM,to:'/',nm:'Sam'},
   {id:'in_theatre',to:'/movies/in_theatre',nm:'Mainland'},
   {id:'hongkong',to:'/movies/hongkong',nm:'Hongkong'},
   {id:'popular',to:'/movies/popular',nm:'Popular'},
   {id:'imdb_boxoffice',to:'/movies/imdb_boxoffice',nm:'IMDB Box Office'}
 ]
+const getRouteStyleClass = (routeName, routeId) => {
+  return `menu-item ${routeName == routeId?'active':''}`;
+}
 watch(() => route.params, () => {
-  routeName.value = route.params?.name;
+  routeName.value = getRouteName(route);
 // Optionally you can set immediate: true config for the watcher to run on init
 //}, { immediate: true });
 });
@@ -19,7 +26,7 @@ watch(() => route.params, () => {
   <section>
       <header class="mt-2 mb-4">
         <nav class="flex justify-center gap-x-2">
-          <NuxtLink v-for="route in ROUTES" :class="`menu-item ${routeName == route.id?'active':''}`" :to="route.to">{{ route.nm }}</NuxtLink>
+          <NuxtLink v-for="route in ROUTES" :class="getRouteStyleClass(routeName, route.id)" :to="route.to">{{ route.nm }}</NuxtLink>
         </nav>
       </header>
     <NuxtLayout>
