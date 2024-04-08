f145873d464ea7aec02cc0806f3cc97131528b55
09liweis <weisen.li@hotmail.com>
2024-04-08 08:25:28 -0400

    refactor css style class

--- pages/movies/[name].vue
+++ pages/movies/[name].vue
@@ -2,14 +2,14 @@
   <main class="flex justify-center items-center h-full">
     <Loading color="text-red-500" v-if="loading" />
     <section class="flex flex-wrap" v-else>
-      <article v-for="movie in moviesStore.movieList" class="w-1/2 sm:1/3 md:w-1/4 lg:w-1/6 relative p-2 transition duration-200 hover:scale-105">
+      <article v-for="movie in moviesStore.movieList" class="movie-card">
         <video v-if="movie.video" autoplay loop class="movie-poster">
           <source :src="movie.video" type="video/mp4">
         </video>
         <NuxtImg v-else :src="movie.poster" onerror="this.src=movie.origin_poster" width="100%" :alt="movie.title" placeholder="https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg" loading="lazy" class="movie-poster" />
         <h3>{{ movie.title }}</h3>
         <Rating v-if="movie.douban_id" :rating="movie.douban_rating" :movieLink="movie.origin_url" :title="'Douban'" />
-        <p v-if="movie.release" class="bg-white text-sky-600 rounded px-1 absolute top-2 left-2">{{ movie.release }}</p>
+        <p v-if="movie.release" class="movie-card-year">{{ movie.release }}</p>
       </article>
     </section>
   </main>
