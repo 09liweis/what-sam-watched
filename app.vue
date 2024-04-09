51222e0a82a3d49c4797a88cef6112fadca353cf
09liweis <weisen.li@hotmail.com>
2024-04-09 10:38:02 -0400

    add route constant

--- app.vue
+++ app.vue
@@ -1,20 +1,13 @@
 <script setup>
 import { ref, computed, watch } from 'vue';
+import {SAM, ROUTES} from './constants/route';
 const route = useRoute();
-const SAM = 'sam';
+
 const getRouteName = (route) => {
   return route.params?.name || SAM;
 }
 let routeName = ref(getRouteName(route));
-const ROUTES = [
-  {id:SAM,to:'/',nm:'Sam'},
-  {id:'in_theatre',to:'/movies/in_theatre',nm:'Mainland'},
-  {id:'comming',to:'/movies/comming',nm:'In comming'},
-  {id:'cineplex',to:'/movies/cineplex',nm:'Cineplex'},
-  {id:'hongkong',to:'/movies/hongkong',nm:'Hongkong'},
-  {id:'popular',to:'/movies/popular',nm:'Popular'},
-  {id:'imdb_boxoffice',to:'/movies/imdb_boxoffice',nm:'IMDB Box Office'}
-]
+
 const getRouteStyleClass = (routeName, routeId) => {
   return `menu-item ${routeName == routeId?'active':''}`;
 }
---
+++ constants/route.ts
@@ -0,0 +1,11 @@
+export const SAM = 'sam';
+
+export const ROUTES = [
+    {id:SAM,to:'/',nm:'Sam'},
+    {id:'in_theatre',to:'/movies/in_theatre',nm:'Mainland'},
+    {id:'comming',to:'/movies/comming',nm:'In comming'},
+    {id:'cineplex',to:'/movies/cineplex',nm:'Cineplex'},
+    {id:'hongkong',to:'/movies/hongkong',nm:'Hongkong'},
+    {id:'popular',to:'/movies/popular',nm:'Popular'},
+    {id:'imdb_boxoffice',to:'/movies/imdb_boxoffice',nm:'IMDB Box Office'}
+  ]
