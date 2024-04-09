41ad71b1fe83a9c7e71e940a756931f1427d656e
09liweis <weisen.li@hotmail.com>
2024-04-09 10:15:40 -0400

    add cineplex route, fix poster

---
+++ constants/movie.ts
@@ -0,0 +1,1 @@
+export const POSTER_DEFAULT:string = "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg";
--- pages/movies/[name].vue
+++ pages/movies/[name].vue
@@ -6,7 +6,7 @@
         <video v-if="movie.video" autoplay loop class="movie-poster">
           <source :src="movie.video" type="video/mp4">
         </video>
-        <NuxtImg v-else :src="movie.poster" onerror="this.src=movie.origin_poster" width="100%" :alt="movie.title" placeholder="https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg" loading="lazy" class="movie-poster" />
+        <NuxtImg v-else :src="movie.poster || POSTER_DEFAULT" width="100%" :alt="movie.title" :placeholder="POSTER_DEFAULT" loading="lazy" class="movie-poster" />
         <h3>{{ movie.title }}</h3>
         <Rating v-if="movie.douban_id" :rating="movie.douban_rating" :movieLink="movie.origin_url" :title="'Douban'" />
         <p v-if="movie.release" class="movie-card-year">{{ movie.release }}</p>
@@ -18,6 +18,7 @@
 <script setup>
   import Loading from '../components/Loading';
   import Rating from '../components/Rating';
+  import {POSTER_DEFAULT} from '../../constants/movie';
   import { onMounted, ref } from 'vue';
   import { useMoviesStore } from '@/stores/movies';
   const runtimeConfig = useRuntimeConfig();
