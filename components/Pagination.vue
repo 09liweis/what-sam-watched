447a8fc67fbe8181f5d57460f2ed4966bd396423
09liweis <weisen.li@hotmail.com>
2024-04-22 12:35:50 -0400

    add active style to pagination

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,9 +1,15 @@
 <script setup>
 import { useMoviesStore } from '@/stores/movies';
 const moviesStore = useMoviesStore();
+const curPage = moviesStore.page;
+
+const getClass = (curPage, page) => {
+  return `menu-item min-w-7 text-center ${curPage == page?'active':''}`;
+}
+
 </script>
 <template>
   <section class="flex flex-wrap items-center justify-center gap-2">
-    <NuxtLink v-for="(page) in moviesStore.pages" :to="`?page=${page+1}`" class="menu-item min-w-7 text-center">{{ page + 1 }}</NuxtLink>
+    <NuxtLink v-for="(page) in moviesStore.pages" :to="`?page=${page+1}`" :class="getClass(curPage, page+1)">{{ page + 1 }}</NuxtLink>
   </section>
 </template>
