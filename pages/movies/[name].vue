1ac595b81448c883eb75cf3eba468b5c0b3fbbf4
09liweis <weisen.li@hotmail.com>
2024-05-07 11:57:40 -0400

    refactor movieCards

--- pages/movies/[name].vue
+++ pages/movies/[name].vue
@@ -2,25 +2,16 @@
   <main class="flex justify-center items-center h-full">
     <Loading color="text-red-500" v-if="loading" />
     <section class="flex flex-wrap" v-else>
-      <article v-for="movie in moviesStore.movieList" class="movie-card">
-        <video v-if="movie.video" autoplay loop class="movie-poster">
-          <source :src="movie.video" type="video/mp4">
-        </video>
-        <NuxtImg v-else :src="movie.poster || POSTER_DEFAULT" width="100%" :alt="movie.title" :placeholder="POSTER_DEFAULT" loading="lazy" class="movie-poster" />
-        <h3>{{ movie.title }}</h3>
-        <Rating v-if="movie.douban_id" :rating="movie.douban_rating" :movieLink="movie.origin_url" :title="'Douban'" />
-        <p v-if="movie.release" class="movie-card-year">{{ movie.release }}</p>
-      </article>
+      <MovieCards />
     </section>
   </main>
 </template>
   <style></style>
 <script setup>
   import Loading from '../components/Loading';
-  import Rating from '../components/Rating';
-  import {POSTER_DEFAULT} from '../../constants/movie';
   import { onMounted, ref } from 'vue';
   import { useMoviesStore } from '@/stores/movies';
+  import MovieCards from '../../components/movie/MovieCards.vue';
   const runtimeConfig = useRuntimeConfig();
   
   const route = useRoute();
