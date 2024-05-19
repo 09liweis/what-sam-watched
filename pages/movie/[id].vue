1d6544a7bd2f025ab2ecf3cea749db7dd32bb06b
09liweis <weisen.li@hotmail.com>
2024-05-18 20:24:02 -0400

    display movie detail photos

--- pages/movie/[id].vue
+++ pages/movie/[id].vue
@@ -1,15 +1,22 @@
 <template>
   <main>
-    <Loading v-if="loading" />
-    <section class="flex" v-if="movie">
-      <NuxtImg v-if="movie?.poster" :src="movie.poster" width="30%" class="" />
-      <section>
-        <h1 class="mr-2 shadow bg-indigo-500 text-white p-1 flex rounded-md">
-          {{ movie.title }}
-        </h1>
-        <p>{{ movie.imdb_id }}</p>
-        <p>{{ movie.summary }}</p>
-        <p>{{ movie.douban_rating }}</p>
+    <Loading v-if="loading" color="text-red" />
+    <section>
+      <section class="flex" v-if="movie">
+        <NuxtImg v-if="movie?.poster" :src="movie.poster" width="30%" class="" />
+        <section>
+          <h1 class="mr-2 shadow bg-indigo-500 text-white p-1 flex rounded-md">
+            {{ movie.title }}
+          </h1>
+          <p>{{ movie.imdb_id }}</p>
+          <p>{{ movie.summary }}</p>
+          <p>{{ movie.douban_rating }}</p>
+        </section>
+      </section>
+      <section class="flex mt-3">
+        <article v-for="photo in movie.photos" class="w-1/5 gap-x-2">
+          <NuxtImg :src="photo.src" width="100%" class="rounded" />
+        </article>
       </section>
     </section>
   </main>
@@ -31,10 +38,10 @@ let loading = ref(false);
 onMounted(async () => {
   loading.value = true;
   const responseData = await $fetch(
-    `https://samliweisen.onrender.com/api/movie/${route.params.id}?imgserver=img9`
+    `https://samliweisen.onrender.com/api/movie/douban/${route.params.id}?imgserver=img9`
   );
   loading.value = false;
-  movie = responseData.movie;
+  movie = responseData;
   moviesStore.setCurrentMovie(movie);
 });
 </script>
