7f33578022ff1048e8ce1e6143f93819c5c5c05f
09liweis <weisen.li@hotmail.com>
2024-06-21 09:15:28 -0400

    add detail movie link to movie card

--- components/movie/MovieCards.vue
+++ components/movie/MovieCards.vue
@@ -6,13 +6,18 @@ const moviesStore = useMoviesStore();
 </script>
 <template>
   <article v-for="movie in moviesStore.movieList" class="movie-card">
-    <video v-if="movie.video" autoplay loop class="movie-poster">
-      <source :src="movie.video" type="video/mp4">
-    </video>
-    <NuxtImg v-else :src="movie.poster || POSTER_DEFAULT" width="100%" :alt="movie.title" :placeholder="POSTER_DEFAULT" loading="lazy" class="movie-poster" />
-    <h3>{{ movie.title }}</h3>
-    <Rating v-if="movie.douban_id" :rating="movie.douban_rating" :movieLink="movie.origin_url" :title="'Douban'" />
-    <p v-if="movie.release" class="movie-card-year">{{ movie.release }}</p>
+    <NuxtLink
+        @click="moviesStore.setCurrentMovie(movie)"
+        :to="`/movie/${movie.douban_id}`"
+    >
+      <video v-if="movie.video" autoplay loop class="movie-poster">
+        <source :src="movie.video" type="video/mp4">
+      </video>
+      <NuxtImg v-else :src="movie.poster || POSTER_DEFAULT" width="100%" :alt="movie.title" :placeholder="POSTER_DEFAULT" loading="lazy" class="movie-poster" />
+      <h3>{{ movie.title }}</h3>
+      <Rating v-if="movie.douban_id" :rating="movie.douban_rating" :movieLink="movie.origin_url" :title="'Douban'" />
+      <p v-if="movie.release" class="movie-card-year">{{ movie.release }}</p>
+    </NuxtLink>
   </article>
 </template>
 <style scoped>
