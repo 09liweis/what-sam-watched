8251ea4965ed3a404ad52caf98e2268d7bc269a0
09liweis <weisen.li@hotmail.com>
2024-07-19 14:50:17 -0400

    create new componet LinkButton

---
+++ components/LinkButton.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  props: ['text','to','cssClass'],
+  setup(props) {
+    // setup() receives props as the first argument.
+  }
+}
+</script>
+<template>
+  <NuxtLink :class="cssClass" :to="to">{{ text }}</NuxtLink>
+</template>
--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useMoviesStore } from '@/stores/movies';
+import LinkButton from '@/components/LinkButton';
 const moviesStore = useMoviesStore();
 
 const getClass = (curPage, page) => {
@@ -9,6 +10,6 @@ const getClass = (curPage, page) => {
 </script>
 <template>
   <section class="flex flex-wrap items-center justify-center gap-2 mb-2">
-    <NuxtLink v-for="(page) in moviesStore.pages" :class="getClass(moviesStore.page, page)" :to="{query:{page:page}}">{{ page }}</NuxtLink>
+    <LinkButton v-for="(page) in moviesStore.pages" :class="getClass(moviesStore.page, page)" :to="{query:{page}}" :text="page"></LinkButton>
   </section>
 </template>
