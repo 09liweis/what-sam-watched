9c02bc4cba37374b8c90c70aff09b6a511ccc932
09liweis <weisen.li@hotmail.com>
2024-07-24 14:53:29 -0400

    add icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,7 +31,10 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
-      charset: 'utf-16',
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.ico" }
+      ],
+      charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       title: 'What Sam Watched',
       meta: [{ name: 'description', content: 'My amazing movie dashboard.' }],
