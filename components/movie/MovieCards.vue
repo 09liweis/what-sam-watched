ff1bb716db8e7bc79175063ed7a3b08e0f8bc8b8
09liweis <weisen.li@hotmail.com>
2024-10-07 11:55:47 -0400

    update movie cards layout with grid

--- components/movie/MovieCards.vue
+++ components/movie/MovieCards.vue
@@ -5,7 +5,7 @@ import { useMoviesStore } from '@/stores/movies';
 const moviesStore = useMoviesStore();
 </script>
 <template>
-  <section class="flex flex-wrap">
+  <section class="grid gap-1 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
     <article v-for="movie in moviesStore.movieList" class="movie-card">
       <NuxtLink
           @click="moviesStore.setCurrentMovie(movie)"
