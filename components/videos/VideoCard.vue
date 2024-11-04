41f12e78c607ccf6fd8e0554addcf144f12fbc3b
09liweis <weisen.li@hotmail.com>
2024-11-04 13:36:42 -0500

    display video length

--- components/videos/VideoCard.vue
+++ components/videos/VideoCard.vue
@@ -11,5 +11,6 @@ export default {
   <section class="w-1/2 sm:w-1/4 md:w-1/5 lg:w-1/6" @click="setCurVideo(video)">
     <Image :src="video.photo" :alt="video.title" :classes="''" />
     <h4 class="text-sm">{{video.title}}</h4>
+    <p class="font-bold">{{ video.length }}</p>
   </section>
 </template>
