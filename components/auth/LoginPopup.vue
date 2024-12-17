3919a9e45f5e6cfff55e2c8feb5c21d048543846
09liweis <weisen.li@hotmail.com>
2024-12-17 15:06:23 -0500

    fix login popup style

--- components/auth/LoginPopup.vue
+++ components/auth/LoginPopup.vue
@@ -22,7 +22,7 @@ defineEmits(['close', 'login']);
     leave-to-class="opacity-0 scale-95"
   >
     <div v-if="show" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-      <div class="flex min-h-screen items-end justify-center px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+      <div class="flex min-h-screen items-center justify-center px-4 pt-4 pb-20 text-center sm:block sm:p-0">
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true" @click="$emit('close')"></div>
 
         <span class="hidden sm:inline-block sm:h-screen sm:align-middle" aria-hidden="true">&#8203;</span>
