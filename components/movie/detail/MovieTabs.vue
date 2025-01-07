194971c03b8b2f311db35b219c116ee800dadbc3
09liweis <weisen.li@hotmail.com>
2025-01-06 19:44:47 -0500

    capitalize movie tabs

--- components/movie/detail/MovieTabs.vue
+++ components/movie/detail/MovieTabs.vue
@@ -20,7 +20,7 @@ defineEmits(['tabChange']);
       :key="key"
       @click="$emit('tabChange', api, key)"
       :class="[
-        'px-4 py-2 font-medium rounded-t-lg transition-colors duration-200',
+        'px-4 py-2 font-medium rounded-t-lg transition-colors duration-200 capitalize',
         currentTab === key 
           ? 'text-indigo-600 border-b-2 border-indigo-600 bg-indigo-50' 
           : 'text-gray-600 hover:text-indigo-600 hover:bg-indigo-50'
