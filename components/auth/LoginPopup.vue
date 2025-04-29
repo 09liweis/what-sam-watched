ba205f7bb6729be6dcdb2af8e81bfe67544314be
09liweis <weisen.li@hotmail.com>
2025-04-29 13:44:32 -0400

    fix login form text align

--- components/auth/LoginPopup.vue
+++ components/auth/LoginPopup.vue
@@ -42,7 +42,7 @@ defineEmits(['close', 'login']);
           </div>
 
           <div class="sm:flex sm:items-start">
-            <div class="mt-3 w-full text-center sm:mt-0 sm:text-left">
+            <div class="mt-3 w-full">
               <h3 class="text-lg font-medium leading-6 text-gray-900 mb-4" id="modal-title">Sign In</h3>
               <LoginForm @close="$emit('close')" @login="$emit('login', $event)" />
             </div>
