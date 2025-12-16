a58e6555a339ce227a17517d4d349532b975eeac
Sam Li <weisen.li@hotmail.com>
2025-12-16 03:51:08 +0000

    style movie poster border raidus

--- components/movie/MovieCard.vue
+++ components/movie/MovieCard.vue
@@ -1,15 +1,15 @@
 <script setup>
-import Rating from '@/components/Rating';
-import MovieEpisodeBtn from '@/components/movie/MovieEpisodeBtn';
-import MoviePoster from '@/components/movie/MoviePoster';
-import { useMoviesStore } from '@/stores/movies';
-import { useAuthStore } from '~/stores/auth';
+import Rating from "@/components/Rating";
+import MovieEpisodeBtn from "@/components/movie/MovieEpisodeBtn";
+import MoviePoster from "@/components/movie/MoviePoster";
+import { useMoviesStore } from "@/stores/movies";
+import { useAuthStore } from "~/stores/auth";
 
 defineProps({
   movie: {
     type: Object,
-    required: true
-  }
+    required: true,
+  },
 });
 
 const authStore = useAuthStore();
@@ -23,9 +23,16 @@ const moviesStore = useMoviesStore();
       :to="`/movie/${movie.douban_id}`"
       class="block overflow-hidden"
     >
-      <div class="relative overflow-hidden rounded-lg shadow-lg transition-all duration-300 group-hover:shadow-xl">
-        <video v-if="movie.video" autoplay loop class="w-full aspect-[2/3] object-cover">
-          <source :src="movie.video" type="video/mp4">
+      <div
+        class="relative overflow-hidden rounded shadow transition-all duration-300 group-hover:shadow-xl"
+      >
+        <video
+          v-if="movie.video"
+          autoplay
+          loop
+          class="w-full aspect-[2/3] object-cover"
+        >
+          <source :src="movie.video" type="video/mp4" />
         </video>
         <MoviePoster
           v-else
@@ -33,39 +40,44 @@ const moviesStore = useMoviesStore();
           :alt="movie.title"
           className="aspect-[2/3] transition-transform duration-300 group-hover:scale-105"
         />
-        <div class="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent opacity-0 transition-opacity duration-300 group-hover:opacity-100"></div>
-        <span v-if="movie.release||movie.year" class="movie-card-year">
-          {{ movie.release||movie.year }}
+        <div
+          class="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+        ></div>
+        <span v-if="movie.release || movie.year" class="movie-card-year">
+          {{ movie.release || movie.year }}
         </span>
       </div>
-      <h3 :title="movie.title" class="mt-2 text-lg font-semibold text-gray-800 line-clamp-1 group-hover:text-red-500">
+      <h3
+        :title="movie.title"
+        class="mt-2 text-lg font-semibold text-gray-800 line-clamp-1 group-hover:text-red-500"
+      >
         {{ movie.title }}
       </h3>
     </NuxtLink>
-    
+
     <div class="mt-2 space-y-1">
-      <Rating 
-        v-if="movie.douban_id" 
-        :rating="movie.douban_rating" 
-        :movieLink="movie.origin_url" 
-        :title="'Douban'" 
+      <Rating
+        v-if="movie.douban_id"
+        :rating="movie.douban_rating"
+        :movieLink="movie.origin_url"
+        :title="'Douban'"
       />
-      <Rating 
-        v-if="movie.imdb_id" 
-        :rating="movie.imdb_rating" 
-        :movieLink="movie.imdb_url" 
-        :title="'IMDB'" 
+      <Rating
+        v-if="movie.imdb_id"
+        :rating="movie.imdb_rating"
+        :movieLink="movie.imdb_url"
+        :title="'IMDB'"
       />
       <p v-if="movie.currentGross" class="text-sm text-gray-600">
         Current: <span class="font-mtsi" v-html="movie.currentGross"></span>
       </p>
       <p v-if="movie.totalGross" class="text-sm text-gray-600">
-        Total: {{movie.totalGross}}
+        Total: {{ movie.totalGross }}
       </p>
-      <MovieEpisodeBtn 
-        v-if="movie._id && authStore.isAuthenticated" 
-        :movie="movie" 
-        :updateEpisode="moviesStore.updateEpisode" 
+      <MovieEpisodeBtn
+        v-if="movie._id && authStore.isAuthenticated"
+        :movie="movie"
+        :updateEpisode="moviesStore.updateEpisode"
       />
       <button
         v-if="movie._id && authStore.isAuthenticated"
