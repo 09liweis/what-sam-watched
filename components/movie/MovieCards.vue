282d6fc426902c0aab3a0708b044c6200393dede
Sam Li <weisen.li@hotmail.com>
2026-02-04 21:08:39 +0000

    update movie list grid

--- components/movie/MovieCards.vue
+++ components/movie/MovieCards.vue
@@ -1,15 +1,17 @@
 <script setup>
-import { useMoviesStore } from '@/stores/movies';
-import MovieCard from '@/components/movie/MovieCard.vue';
+import { useMoviesStore } from "@/stores/movies";
+import MovieCard from "@/components/movie/MovieCard.vue";
 const moviesStore = useMoviesStore();
 </script>
 
 <template>
-  <section class="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-    <MovieCard 
-      v-for="movie in moviesStore.movieList" 
-      :key="movie.douban_id" 
-      :movie="movie" 
+  <section
+    class="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5"
+  >
+    <MovieCard
+      v-for="movie in moviesStore.movieList"
+      :key="movie.douban_id"
+      :movie="movie"
     />
   </section>
 </template>
