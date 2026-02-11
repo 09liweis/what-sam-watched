0dfb84af98821e25fd4aed22877148ff0a01fbdf
Sam Li <weisen.li@hotmail.com>
2026-02-11 21:56:31 +0000

    refactor Pagination

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,15 +1,19 @@
 <script setup>
-import { useMoviesStore } from '@/stores/movies';
-import LinkButton from '@/components/LinkButton';
+import { useMoviesStore } from "@/stores/movies";
+import LinkButton from "@/components/LinkButton";
 const moviesStore = useMoviesStore();
 
 const getClass = (curPage, page) => {
-  return `menu-item ${curPage == page?'active':''}`;
-}
-
+  return `menu-item ${curPage == page ? "active" : ""}`;
+};
 </script>
 <template>
   <section class="flex flex-wrap items-center justify-center gap-2 mb-2">
-    <LinkButton v-for="(page) in moviesStore.pages" :class="getClass(moviesStore.page, page)" :to="{query:{page}}" :text="page"></LinkButton>
+    <LinkButton
+      v-for="page in moviesStore.pages"
+      :class="getClass(moviesStore.page, page)"
+      :to="{ query: { page } }"
+      :text="page"
+    />
   </section>
 </template>
