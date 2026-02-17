1425ba70df0e1fc8d0c6062cc298408414a2a5cf
SiriYeung <119325339+SiriYeung@users.noreply.github.com>
2026-02-17 16:28:52 +0000

    scroll to top on linkbutton

--- components/LinkButton.vue
+++ components/LinkButton.vue
@@ -1,11 +1,21 @@
-<script>
-export default {
-  props: ['text','to','cssClass'],
-  setup(props) {
-    // setup() receives props as the first argument.
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  text: String,
+  to: [String, Object],
+  cssClass: String,
+});
+
+const scrollToTop = () => {
+  if (typeof window !== 'undefined') {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
-}
+};
 </script>
+
 <template>
-  <NuxtLink :class="cssClass" :to="to">{{ text }}</NuxtLink>
+  <NuxtLink :class="cssClass" :to="to" @click="scrollToTop">
+    {{ text }}
+  </NuxtLink>
 </template>
